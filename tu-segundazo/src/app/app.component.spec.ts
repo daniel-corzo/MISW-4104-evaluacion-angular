0364030b5615a6a63a7041a9f24dc0311e00acf2
Daniel Corzo <d.corzos@uniandes.edu.co>
2024-11-03 08:17:53 -0500

    Implementar punto 3, testing del componente

--- tu-segundazo/src/app/app.component.spec.ts
+++ tu-segundazo/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideHttpClientTesting(), provideHttpClient()]
     }).compileComponents();
   });
 
@@ -12,18 +15,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'tu-segundazo' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tu-segundazo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tu-segundazo');
   });
 });
--- tu-segundazo/src/app/vehiculos/listar-vehiculo/listar-vehiculo.component.spec.ts
+++ tu-segundazo/src/app/vehiculos/listar-vehiculo/listar-vehiculo.component.spec.ts
@@ -1,23 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListarVehiculoComponent } from './listar-vehiculo.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Vehiculo } from '../vehiculo.model';
+import { VehiculosService } from '../vehiculos.service';
+import { of } from 'rxjs';
 
 describe('ListarVehiculoComponent', () => {
   let component: ListarVehiculoComponent;
   let fixture: ComponentFixture<ListarVehiculoComponent>;
+  let vehiculosService: VehiculosService;
+  let mockVehiculos: Vehiculo[];
 
   beforeEach(async () => {
+    mockVehiculos = [
+      { id: 1, marca: 'Renault', linea: 'Kangoo', referencia: 'VU Express', modelo: 2017, kilometraje: 93272, color: 'Blanco', imagen: 'https://cdn.group.renault.com/ren/co/vehicles/kangoo/home/renault-kangoo-exterior.jpg' },
+      { id: 2, marca: 'Chevrolet', linea: 'Spark', referencia: 'Life', modelo: 2018, kilometraje: 55926, color: 'Plata', imagen: 'https://turistran.com/2-thickbox_default/chevrolet-spark-life.jpg' },
+      { id: 3, marca: 'Chevrolet', linea: 'Sail', referencia: 'LT Sedan', modelo: 2016, kilometraje: 94321, color: 'Rojo', imagen: 'https://www.chevrolet.com.ec/content/dam/chevrolet/south-america/ecuador/espanol/index/cars/2019-sail/mov/01-images/2018-chevrolet-sail-rojo-01.png' }
+    ];
+
     await TestBed.configureTestingModule({
-      imports: [ListarVehiculoComponent]
-    })
-    .compileComponents();
+      imports: [ListarVehiculoComponent],
+      providers: [
+        VehiculosService,
+        provideHttpClientTesting(),
+        {
+          provide: HttpClient,
+          useValue: {
+            get: () => of(mockVehiculos)
+          }
+        }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ListarVehiculoComponent);
     component = fixture.componentInstance;
+    vehiculosService = TestBed.inject(VehiculosService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a table with 3 rows', async () => {
+    fixture.detectChanges();
+    expect(component.vehiculos).toEqual(mockVehiculos);
+    
+    const compiled = fixture.nativeElement;
+    const rows = compiled.querySelectorAll('tbody tr');
+    expect(rows.length).toBe(3);
   });
 });
